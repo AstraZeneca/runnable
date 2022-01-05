dfb274c4c6bf205a28f97da5ec30cad37a08df5e
Vijay Vammi <vijay.vammi@astrazeneca.com>
2022-01-05 16:57:29 +0000

    WIP: Working on catalog module

--- magnus/catalog.py
+++ magnus/catalog.py
@@ -1,8 +1,19 @@
+"""
+Magnus core service - Catalog.
 
+get_run_log_store - Returns the instance of the run log store used by run
+is_catalog_out_of_sync - return True if a particular catalog object differs from the one versioned already.
+
+BaseCatalog - The base catalog class definition.
+DummyCatalog - A dummy catalog implementation that does nothing.
+FileSystemCatalog - A catalog implementation using the file system
+"""
 import logging
 from pathlib import Path
 import shutil
 import os
+
+from typing import List
 
 
 from magnus import defaults
@@ -32,33 +43,46 @@ def is_catalog_out_of_sync(catalog, synced_catalogs=None) -> bool:
     If the object does not exist or synced catalog does not exist, return True
     """
     if not synced_catalogs:
-        synced_catalogs = []
+        return True  # If nothing has been synced in the past
+
     for synced_catalog in synced_catalogs:
         if synced_catalog.catalog_relative_path == catalog.catalog_relative_path:
             if synced_catalog.data_hash == catalog.data_hash:
                 return False
             return True
-    return True
+
+    return True  # The object does not exist, sync it
 
 
 class BaseCatalog:
     """
-    A Base Catalog handler class
+    Base Catalog class definition.
 
     All implementations of the catalog handler should inherit and extend this class.
 
-    As a general design guideline, do not instantiate anything from config as they might change.
-    Instead have getters methods to get variables from the config.
+    Note: As a general guideline, do not extract anything from the config to set class level attributes.
+          Intergration patterns modify the config after init to change behaviors.
+          Access config properties using getters of the class.
     """
     service_name = None
 
     def __init__(self, config, **kwargs):  # pylint: disable=unused-argument
         self.config = config or {}
-        self.compute_data_folder = self.config.get('compute_data_folder', defaults.COMPUTE_DATA_FOLDER)
 
-    def get(self, name: str, run_id: str, compute_data_folder=None, **kwargs):  # pylint: disable=unused-argument
+    @property
+    def compute_data_folder(self) -> str:
         """
-        Get the catalog item by 'name' for the run id, 'run_id' and store it in compute data folder
+        Returns the compute data folder defined as per the config of the catalog.
+
+        Returns:
+            [str]: The compute data folder as defined or defaults to magnus default 'data/'
+        """
+        return self.config.get('compute_data_folder', defaults.COMPUTE_DATA_FOLDER)
+
+    def get(self, name: str, run_id: str, compute_data_folder=None, **kwargs) -> List[object]:
+        # pylint: disable=unused-argument
+        """
+        Get the catalog item by 'name' for the 'run id' and store it in compute data folder
 
         Args:
             name (str): The name of the catalog item
@@ -67,10 +91,13 @@ class BaseCatalog:
 
         Raises:
             NotImplementedError: Base class, hence not implemented
+
+        Returns:
+            List(object) : A list of catalog objects
         """
         raise NotImplementedError
 
-    def put(self, name: str, run_id: str, compute_data_folder=None, synced_catalogs=None, **kwargs):
+    def put(self, name: str, run_id: str, compute_data_folder=None, synced_catalogs=None, **kwargs) -> List[object]:
         # pylint: disable=unused-argument
         """
         Put the file by 'name' from the 'compute_data_folder' in the catalog for the run_id.
@@ -78,13 +105,16 @@ class BaseCatalog:
         If previous syncing has happened and the file has not been changed, we do not sync again.
 
         Args:
-            name (str): [description]
-            run_id (str): [description]
+            name (str): The name of the catalog item.
+            run_id (str): The run_id of the run.
             compute_data_folder (str, optional): The compute data folder. Defaults to magnus default (data/)
             synced_catalogs (dict, optional): Any previously synced catalogs. Defaults to None.
 
         Raises:
             NotImplementedError: Base class, hence not implemented
+
+        Returns:
+            List(object) : A list of catalog objects
         """
         raise NotImplementedError
 
@@ -102,35 +132,6 @@ class BaseCatalog:
         raise NotImplementedError
 
 
-def get_catalog_handler(config: dict) -> BaseCatalog:
-    """
-    Get the implementation of the catalog handler as per the config.
-
-    We give a file-system catalog if none is provided
-
-    Args:
-        config (dict): The implemented catalog handler class
-
-    Raises:
-        Exception: If config is provided with no type in defined.
-
-    Returns:
-        BaseCatalog: An implemented catalog handler
-    """
-    if config:
-        catalog_type = config.get('type', None)
-        if not catalog_type:
-            raise Exception('Catalog type is necessary in catalog config')
-
-        logger.info(f'Trying to get a Catalog handler of type: {catalog_type}')
-        for sub_class in BaseCatalog.__subclasses__():
-            if catalog_type == sub_class.service_name:
-                return sub_class(config.get('config', {}))
-
-    logger.warning('Getting a File System catalog type')
-    return FileSystemCatalog(config={})
-
-
 class DummyCatalog(BaseCatalog):
     """
     A Catalog handler that does nothing.
@@ -143,50 +144,25 @@ class DummyCatalog(BaseCatalog):
     """
     service_name = 'dummy'
 
-    def get(self, name: str, run_id: str, compute_data_folder=None, **kwargs):
-        """ Get the file by matching glob pattern to the name
-
-        Args:
-            name ([str]): A glob matching the file name
-            run_id ([str]): The run id
-
-        Raises:
-            Exception: If the catalog location does not exist
+    def get(self, name: str, run_id: str, compute_data_folder=None, **kwargs) -> List[object]:
         """
-        ...
-
-    def put(self, name: str, run_id: str, compute_data_folder=None, synced_catalogs=None, **kwargs):
+        Does nothing as it is a dummy catalog handler
         """
-        Put the files matching the glob pattern into the catalog.
+        logger.info('Using a dummy catalog, doing nothing in get')
+        return []
 
-        If previously synced catalogs are provided, and no changes were observed, we do not sync them.
-
-        Args:
-            name (str): The glob pattern of the files to catalog
-            run_id (str): The run id of the run
-            compute_data_folder (str, optional): The compute data folder to sync from. Defaults to settings default.
-            synced_catalogs (dict, optional): dictionary of previously synced catalogs. Defaults to None.
-
-        Raises:
-            Exception: If the compute data folder does not exist.
-
-        Returns:
-            [type]: [description]
+    def put(self, name: str, run_id: str, compute_data_folder=None, synced_catalogs=None, **kwargs) -> List[object]:
         """
-        ...
+        Does nothing as it is a dummy catalog handler.
+        """
+        logger.info('Using a dummy catalog, doing nothing in put')
+        return []
 
     def sync_between_runs(self, previous_run_id: str, run_id: str):
         """
-        Given the previous run id, sync the catalogs between the current one and previous
-
-        Args:
-            previous_run_id (str): The previous run id to sync the catalogs from
-            run_id (str): The run_id to which the data catalogs should be synced to.
-
-        Raises:
-            Exception: If the previous run log does not exist in the catalog
-
+        Does nothing as it a dummy catalog hander.
         """
+        logger.info('Using a dummy catalog, doing nothing while sync between runs')
         ...
 
 
@@ -194,7 +170,7 @@ class FileSystemCatalog(BaseCatalog):
     """
     A Catalog handler that uses the local file system for cataloging.
 
-    Do not use this catalog if the compute is not local.
+    Note: Do not use this if the steps of the pipeline run on different compute environments.
 
     Example config:
 
@@ -206,23 +182,19 @@ class FileSystemCatalog(BaseCatalog):
 
     """
     # TODO think of compression algorithms to save disk space, ZipFile
-    # TODO add map_variable as glob pattern
-    # TODO: consider run_id as a glob pattern
     service_name = 'file-system'
 
-    def get_catalog_location(self):
+    def get_catalog_location(self) -> str:
         """
         Get the catalog location from the config.
         If its not defined, use the magnus default
 
         Returns:
-            str: The catalog location
+            str: The catalog location as defined by the config or magnus default '.catalog'
         """
-        catalog_location = self.config.get('catalog_location', defaults.CATALOG_LOCATION_FOLDER)
+        return self.config.get('catalog_location', defaults.CATALOG_LOCATION_FOLDER)
 
-        return catalog_location
-
-    def get(self, name: str, run_id: str, compute_data_folder=None, **kwargs):
+    def get(self, name: str, run_id: str, compute_data_folder=None, **kwargs) -> List[object]:
         """ Get the file by matching glob pattern to the name
 
         Args:
@@ -231,7 +203,12 @@ class FileSystemCatalog(BaseCatalog):
 
         Raises:
             Exception: If the catalog location does not exist
+
+        Returns:
+            List(object) : A list of catalog objects
         """
+        logger.info(f'Using the {self.service_name} catalog and trying to get {name} for run_id: {run_id}')
+
         copy_to = self.compute_data_folder
         if compute_data_folder:
             copy_to = compute_data_folder
@@ -239,12 +216,22 @@ class FileSystemCatalog(BaseCatalog):
         catalog_location = self.get_catalog_location()
         utils.safe_make_dir(copy_to)
         run_catalog = Path(catalog_location) / run_id
+
+        logger.debug(f'Copying objects to {copy_to} from the run catalog location of {run_catalog}')
+
+        if not utils.does_dir_exist(run_catalog):
+            msg = (
+                f'Expected Catalog to be present at: {run_catalog} but not found.'
+                'Note: Please make sure that some data was put in the catalog before trying to get from it.'
+                'Hint: The starting step of the pipeline cannot get from the catalog'
+            )
+            raise Exception(msg)
+
         # Iterate through the contents of the run_catalog and copy the files that fit the name pattern
         # We should also return a list of data hashes
-        if not utils.does_dir_exist(run_catalog):
-            raise Exception(f'Expected Catalog to be present at: {run_catalog} but not found')
-
         glob_files = run_catalog.glob(name)
+        logger.debug(f'Glob identified {glob_files} as matches to from the catalog location: {run_catalog}')
+
         data_catalogs = []
         run_log_store = get_run_log_store()
         for file in glob_files:
@@ -256,10 +243,11 @@ class FileSystemCatalog(BaseCatalog):
             data_catalogs.append(data_catalog)
 
             shutil.copy(file, copy_to)
+            logger.info(f'Copied {file} from {run_catalog} to {copy_to}')
 
         return data_catalogs
 
-    def put(self, name: str, run_id: str, compute_data_folder=None, synced_catalogs=None, **kwargs):
+    def put(self, name: str, run_id: str, compute_data_folder=None, synced_catalogs=None, **kwargs) -> List[object]:
         """
         Put the files matching the glob pattern into the catalog.
 
@@ -275,8 +263,10 @@ class FileSystemCatalog(BaseCatalog):
             Exception: If the compute data folder does not exist.
 
         Returns:
-            [type]: [description]
+            List(object) : A list of catalog objects
         """
+        logger.info(f'Using the {self.service_name} catalog and trying to put {name} for run_id: {run_id}')
+
         copy_from = self.compute_data_folder
         if compute_data_folder:
             copy_from = compute_data_folder
@@ -284,12 +274,21 @@ class FileSystemCatalog(BaseCatalog):
         catalog_location = self.get_catalog_location()
         run_catalog = Path(catalog_location) / run_id
         utils.safe_make_dir(run_catalog)
+
+        logger.debug(f'Copying objects from {copy_from} to the run catalog location of {run_catalog}')
+
+        if not utils.does_dir_exist(copy_from):
+            msg = (
+                f'Expected compute data folder to be present at: {compute_data_folder} but not found'
+                'Note: Magnus does not create the compute data folder for you. Please ensure that the folder exists'
+            )
+            raise Exception(msg)
+
         # Iterate through the contents of copy_from and if the name matches, we move them to the run_catalog
         # We should also return a list of datastore.DataCatalog items
-        if not utils.does_dir_exist(copy_from):
-            raise Exception(f'Expected compute data folder to be present at: {compute_data_folder} but not found')
-
         glob_files = Path(copy_from).glob(name)
+        logger.debug(f'Glob identified {glob_files} as matches to from the compute data folder: {copy_from}')
+
         data_catalogs = []
         run_log_store = get_run_log_store()
         for file in glob_files:
@@ -319,14 +318,23 @@ class FileSystemCatalog(BaseCatalog):
             Exception: If the previous run log does not exist in the catalog
 
         """
+        logger.info(f'Using the {self.service_name} catalog and syncing catalogs'
+                    'between old: {previous_run_id} to new: {run_id}')
+
         catalog_location = Path(self.get_catalog_location())
         run_catalog = catalog_location / run_id
         utils.safe_make_dir(run_catalog)
 
         if not utils.does_dir_exist(catalog_location / previous_run_id):
-            raise Exception(f'Previous run log by run_id: {previous_run_id} is not found.')
+            msg = (
+                f'Catalogs from previous run : {previous_run_id} are not found.'
+                'Note: Please provision the catalog objects generated by the previous run in the same catalog location'
+                ' as the current run, even if the catalog handler for the previous run was different'
+            )
+            raise Exception(msg)
 
         cataloged_files = (catalog_location / previous_run_id).glob('**/**')
 
         for cataloged_file in cataloged_files:
             shutil.copy(cataloged_file, run_catalog)
+            logger.info(f'Copied file from: {cataloged_file} to {run_catalog}')
