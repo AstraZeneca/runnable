49579ad5ce4b9ed78627ea92d0f746c455460bdc
Vijay Vammi <vijay.vammi@astrazeneca.com>
2023-03-28 13:05:15 +0100

    chore: :arrow_up: increasing doc strings coverage

--- magnus/docker_utils.py
+++ magnus/docker_utils.py
@@ -8,25 +8,35 @@ logger = logging.getLogger(defaults.NAME)
 try:
     import docker
 except ImportError:
-    logger.info('docker was not installed, docker functionality will not work')
+    logger.info("docker was not installed, docker functionality will not work")
 
 
 def generate_docker_file(style: str = "poetry", git_tracked: bool = True):
+    """
+    Generates a Dockerfile based on the given style.
+
+    Args:
+        style (str, optional): One of python dependency style. Defaults to "poetry".
+        git_tracked (bool, optional): Allow only git tracked or not. Defaults to True.
+
+    Raises:
+        Exception: If the current directory is not a git repository.
+    """
     install_style, install_requirements, copy_content = None, None, None
 
-    if style == 'poetry':
-        install_style = 'RUN pip install poetry'
-        install_requirements = 'RUN poetry install'
-        logger.info('Using poetry style for requirements')
-    elif style == 'pipenv':
-        install_style = 'RUN pip install pipenv'
-        install_requirements = 'RUN pipenv install'
-        logger.info('Using pipenv style for requirements')
+    if style == "poetry":
+        install_style = "RUN pip install poetry"
+        install_requirements = "RUN poetry install"
+        logger.info("Using poetry style for requirements")
+    elif style == "pipenv":
+        install_style = "RUN pip install pipenv"
+        install_requirements = "RUN pipenv install"
+        logger.info("Using pipenv style for requirements")
     else:
-        install_requirements = 'RUN pip install -r requirements.txt'
-        logger.info('Trying requirements.txt, if one exists')
+        install_requirements = "RUN pip install -r requirements.txt"
+        logger.info("Trying requirements.txt, if one exists")
 
-    copy_content = 'COPY . /app'
+    copy_content = "COPY . /app"
     if git_tracked:
         if not utils.is_a_git_repo():
             msg = (
@@ -34,26 +44,50 @@ def generate_docker_file(style: str = "poetry", git_tracked: bool = True):
                 "Be aware over-riding this can cause leak of sensitive data if you are not careful."
             )
             raise Exception(msg)
-        copy_content = 'ADD git_tracked.tar.gz /app'
+        copy_content = "ADD git_tracked.tar.gz /app"
         utils.archive_git_tracked(defaults.GIT_ARCHIVE_NAME)
 
     dockerfile_content = Template(defaults.DOCKERFILE_CONTENT).safe_substitute(
-        {'INSTALL_STYLE': install_style, 'INSTALL_REQUIREMENTS': install_requirements,
-         'COPY_CONTENT': copy_content})
-    with open(defaults.DOCKERFILE_NAME, 'w', encoding='utf-8') as fw:
+        {
+            "INSTALL_STYLE": install_style,
+            "INSTALL_REQUIREMENTS": install_requirements,
+            "COPY_CONTENT": copy_content,
+        }
+    )
+    with open(defaults.DOCKERFILE_NAME, "w", encoding="utf-8") as fw:
         fw.write(dockerfile_content)
 
 
-def build_docker(image_name: str, docker_file: Union[str, None],
-                 style: str, tag: str, commit_tag: bool, dry_run: bool = False, git_tracked: bool = True):
+def build_docker(
+    image_name: str,
+    docker_file: Union[str, None],
+    style: str,
+    tag: str,
+    commit_tag: bool,
+    dry_run: bool = False,
+    git_tracked: bool = True,
+):
+    """
+    Builds the docker image.
+
+    Args:
+        image_name (str): The name of the docker image.
+        docker_file (Union[str, None]): The name of the dockerfile, if present or None
+        style (str): The style of python dependency.
+        tag (str): The tag of the docker image.
+        commit_tag (bool): Use the commit id for the docker image tag.
+        dry_run (bool, optional): Do not run just show what would be done. Defaults to False.
+        git_tracked (bool, optional): Only the git tracked files or not. Defaults to True.
+
+    Raises:
+        Exception: If the current directory is not a git repository and asked for a git tag as docker image.
+    """
     if commit_tag:
         if not utils.is_a_git_repo():
-            msg = (
-                "The current project is not git versioned, cannot use commit tag option when building image"
-            )
+            msg = "The current project is not git versioned, cannot use commit tag option when building image"
             raise Exception(msg)
 
-        tag = utils.get_current_code_commit()[:defaults.LEN_SHA_FOR_TAG]  # type: ignore
+        tag = utils.get_current_code_commit()[: defaults.LEN_SHA_FOR_TAG]  # type: ignore
 
     if not docker_file:
         generate_docker_file(style, git_tracked=git_tracked)
@@ -63,4 +97,6 @@ def build_docker(image_name: str, docker_file: Union[str, None],
         return
 
     docker_client = docker.from_env()
-    docker_client.images.build(path=f".", dockerfile=docker_file, tag=f'{image_name}:{tag}', quiet=False)
+    docker_client.images.build(
+        path=f".", dockerfile=docker_file, tag=f"{image_name}:{tag}", quiet=False
+    )
