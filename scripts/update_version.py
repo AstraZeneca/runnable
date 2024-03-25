f3c791afeea7a5267f10f48a89471ce5e8693cb3
Vijay Vammi <vijay.vammi@astrazeneca.com>
2024-03-25 02:21:26 +0000

    chore: isort

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -3,9 +3,7 @@ import sys
 from tomlkit import dump, load
 
 if len(sys.argv) < 2:
-    msg = (
-        "Usage: python upload_version <new_version> <output_to, defaults to pyproject.toml>"
-    )
+    msg = "Usage: python upload_version <new_version> <output_to, defaults to pyproject.toml>"
 
 new_version = sys.argv[1]
 output_file = "pyproject.toml"
@@ -13,6 +11,6 @@ if len(sys.argv) > 2:
     output_file = sys.argv[2]
 
 toml_file = "pyproject.toml"
-contents = load(open(toml_file, 'rb'))
+contents = load(open(toml_file, "rb"))
 contents["tool"]["poetry"]["version"] = str(new_version)
-dump(contents, open(output_file, 'w'))
+dump(contents, open(output_file, "w"))
