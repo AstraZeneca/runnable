3ff686b8e1dd744234dbfb5e670c85cc887b85fd
Vammi, Vijay <vijay.vammi@astrazeneca.com>
2025-01-11 22:40:25 +0000

    fix: cleaned up SDK

--- examples/02-sequential/on_failure_succeed.py
+++ examples/02-sequential/on_failure_succeed.py
@@ -28,10 +28,12 @@ def main():
     step_3 = Stub(name="step 3", terminate_with_success=True)
     step_4 = Stub(name="step 4", terminate_with_success=True)  # (1)
 
-    step_1.on_failure = step_4.name
+    on_failure_pipeline = Pipeline(steps=[step_4])
+
+    step_1.on_failure = on_failure_pipeline  # (2)
 
     pipeline = Pipeline(
-        steps=[step_1, step_2, step_3, [step_4]],
+        steps=[step_1, step_2, step_3],
     )
     pipeline.execute()
 
